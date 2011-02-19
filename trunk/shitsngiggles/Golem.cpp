4391f0a29c7936ffa0e67a1e3c389663695cfabe
jpq0352@gmail.com <jpq0352@gmail.com@cd0932b7-c9a5-d78a-694f-b7cf6240fdf7>
2011-02-19 23:36:56 +0000

    Made golem.cpp, added the virtual functions from object.h to golem.h, and changed playercontroller function to protected

---
+++ trunk/shitsngiggles/Golem.cpp
@@ -0,0 +1,27 @@
+#include "Golem.h"
+
+bool Golem::isDead()
+{
+    
+    return false;
+}
+
+void Golem::prepass(const u32 GameTime)
+{
+    //handle the pre updating in here
+}
+
+void Golem::pass(const u32 GameTime)
+{
+    //handle the update in here
+}
+
+void Golem::postpass(const u32 GameTime)
+{
+    //handle the execution of the update in here
+}
+
+void Golem::handleCollision(Object* Other)
+{
+    //nandle the collision that occurs
+}
