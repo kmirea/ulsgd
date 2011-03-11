8f623f8a7e959da236680f7694628e552bda818a
rooly4@gmail.com <rooly4@gmail.com@cd0932b7-c9a5-d78a-694f-b7cf6240fdf7>
2011-03-11 02:34:22 +0000

    Adding Flight Sim Project starting folder.

---
+++ trunk/FightOrFlight/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: chris
+ *
+ * Created on March 10, 2011, 6:47 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+
+	return 0;
+}
+
