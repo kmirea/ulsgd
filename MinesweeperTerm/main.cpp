8aad69e9faff12a7c6ca166d5b46d28de2bdd076
rooly4 <rooly4@gmail.com>
2011-10-19 23:17:46 +0000

    First Commit for Project Series 1, MinesweeperTerm

---
+++ MinesweeperTerm/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: chris
+ *
+ * Created on October 19, 2011, 6:15 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+
