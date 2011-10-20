44f138ee7d7256f790fc39157d1b13e1c8075f1a
rooly4@gmail.com <rooly4@gmail.com@cd0932b7-c9a5-d78a-694f-b7cf6240fdf7>
2011-10-20 21:51:50 +0000

    Working on MinesweeperTerm
    Chris Parich

--- branches/ChrisParich/MinesweeperTerm/main.cpp
+++ branches/ChrisParich/MinesweeperTerm/main.cpp
@@ -7,14 +7,86 @@
 
 #include <cstdlib>
 #include <iostream>
+#include <vector>
+#include <ctime>
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv)
+
+typedef unsigned int u32;
+typedef int s32;
+
+vector< vector<bool> > Map;
+
+void GenerateMap( u32 X, u32 Y, u32 mines );
+bool SelectCoord( u32 X, u32 Y );
+
+int main()
 {
-	cout << "Hello, world!" << endl;
+	bool isFinished = false;
+	bool isWon = true;
+	
+	u32 mineCount;
+	u32 xDim, yDim;
+	
+	cout << "Please select X and Y Dimensions: " << endl;
+	cout << "X: ";
+	cin >> xDim;
+	cout << "Y: ";
+	cin >> yDim;
+	
+	cout << "How many mines would you like? ";
+	cin >> mineCount;
+	
+	if( xDim * yDim <= mineCount )
+	{
+		cout << "Too many mines, or too small a map :(" << endl;
+		cout << "Restarting..." << endl;
+		return main();
+	}
+	
+	cout << "Generating Map..." << endl;
+	
+	GenerateMap( xDim, yDim, mineCount );
+	
+	while( !isFinished )
+	{
+		
+	}
+	
+	bool playagain = false;
+	
+	if( isWon )
+		cout << "You Won! ";
+	else
+		cout << "Too bad. ";
+	cout << "The game has finished. Would you like to play again (y/n)? ";
+	
 	return 0;
 }
 
+void GenerateMap( u32 X, u32 Y, u32 mines )
+{
+	Map.resize(X, false);
+	for( u32 i=0; i<Map.size(); i++ )
+		Map[i].resize(Y, false);
+	
+	for( u32 i=0; i<Map.size(); i++ )
+	{
+		for( u32 j=0; j<Map[i].size(); j++ )
+		{
+			Map[i][j] = rand()%2;
+			if( Map[i][j] )
+				mines--;
+			if( mines == 0 )
+				return;
+		}
+	}
+}
+
+bool SelectCoord( u32 X, u32 Y )
+{
+	
+}
