9d872bf8ee9972e67d2771f7ec967a0ba11e8962
rooly4@gmail.com <rooly4@gmail.com@cd0932b7-c9a5-d78a-694f-b7cf6240fdf7>
2011-10-20 20:45:39 +0000

    First Commit for Project Series 1, MinesweeperTerm

---
+++ branches/ChrisParich/Minesweeper2D/main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: chris
+ *
+ * Created on October 20, 2011, 3:42 PM
+ */
+
+#include <cstdlib>
+#include <iostream>
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv)
+{
+	cout << "Hello, world!" << endl;
+	return 0;
+}
+
